84a45e445167bd375dab8c65290661f6c6638f24
KeerthiShri01 <keerthisiva0105@gmail.com>
2026-06-23 17:56:30 +0530

    add displayInventory function

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ var inventory = [
   { name: "Dell Alienware m16", inStock: false },
   { name: "Dell Latitude 5540", inStock: true }
 ];
-console.log(inventory);
+function displayInventory() {
+  var list = document.getElementById("product-list");
+  list.innerHTML = "";
+
+  for (var i = 0; i < inventory.length; i++) {
+    var item = inventory[i];
+    var li = document.createElement("li");
+    li.textContent = item.name + " - " + (item.inStock ? "In Stock" : "Out of Stock");
+    list.appendChild(li);
+  }
+}
+
+displayInventory();
