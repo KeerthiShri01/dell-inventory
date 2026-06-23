5333bda7dcfa0aecd838e947bf02fc881a413697
KeerthiShri01 <keerthisiva0105@gmail.com>
2026-06-23 17:44:18 +0530

    add inventory array in app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var inventory = [
+  { name: "Dell XPS 15", inStock: true },
+  { name: "Dell Inspiron 14", inStock: true },
+  { name: "Dell Alienware m16", inStock: false },
+  { name: "Dell Latitude 5540", inStock: true }
+];
+console.log(inventory);
